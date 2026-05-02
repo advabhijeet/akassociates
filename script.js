d5de29895d753270a3e2d4d1ff4ae2b731285dc9
advabhijeet <kadvocateabhijeet@gmail.com>
2026-05-02 21:13:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
-
     document.querySelector(this.getAttribute('href'))
-      .scrollIntoView({
-        behavior: 'smooth'
-      });
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+const sections = document.querySelectorAll("div[id]");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(a => {
+    a.classList.remove("active");
+    if (a.getAttribute("href") === "#" + current) {
+      a.classList.add("active");
+    }
   });
 });
