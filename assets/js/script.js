700f2f233715c909f730f33a00b24a6b413e635c
advabhijeet <281193757+advabhijeet@users.noreply.github.com>
2026-07-03 00:37:11 +0530

    Replace RERA promoter thumbnail with full-frame v3

--- assets/js/script.js
+++ assets/js/script.js
@@ -321,7 +321,7 @@ window.ChambersInsightsRegistryReady = Promise.resolve(window.chambersInsightsRe
     });
 
   const assetPrefix = window.location.pathname.split('/').filter(Boolean).length > 1 ? '../' : '';
-  const registryUrl = assetPrefix + 'assets/data/insights-registry.json?v=registry-7';
+  const registryUrl = assetPrefix + 'assets/data/insights-registry.json?v=registry-8';
 
   window.ChambersInsightsRegistryReady = loadRegistryAsync(registryUrl);
 })();
